18c41866a50e5c504e699db7b8cfc5ff226d4f98
Michal Ziemski <mziemski@ethz.ch>
2026-03-03 22:12:58 +0100

    ENH: add feature-counts action

--- q2_subread/__init__.py
+++ q2_subread/__init__.py
@@ -1,8 +1,8 @@
 # flake8: noqa
 # ----------------------------------------------------------------------------
-# Copyright (c) 2024, Michal Ziemski.
+# Copyright (c) 2026, QIIME 2 development team.
 #
-# Distributed under the terms of the Modified BSD License.
+# Distributed under the terms of the GPL-3.0 license.
 #
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
